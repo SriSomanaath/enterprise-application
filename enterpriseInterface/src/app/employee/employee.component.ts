89539a051f8b1af0eb109d40e9d2901f4bb62137
SriSomanaath <govindalapudisrinath@gmail.com>
2023-07-18 23:04:33 +0530

    template

--- enterpriseInterface/src/app/employee/employee.component.ts
+++ enterpriseInterface/src/app/employee/employee.component.ts
@@ -1,5 +1,8 @@
 import { Component , OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 import { EmployeeService } from '../shared/employee.service';
 
@@ -7,7 +10,9 @@ import { EmployeeService } from '../shared/employee.service';
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css'],
-  providers: [EmployeeService]
+  providers: [EmployeeService],
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
 })
 export class EmployeeComponent implements OnInit{
 
